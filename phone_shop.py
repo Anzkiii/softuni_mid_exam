44d2025d2f8b3332aacffc18b6b0051313dab3c1
Edward <edi.tzekov@gmail.com>
2022-06-26 22:04:33 +0300

    Update phone_shop.py

--- phone_shop.py
+++ phone_shop.py
@@ -9,25 +9,24 @@ while True:
             continue
         else:
             list_of_phones.append(command[2])
-    elif "Remove" in command:
+    if "Remove" in command:
         command = command.split(" ")
         if command[2] in list_of_phones:
             list_of_phones.remove(command[2])
-        else:
-            continue
-    elif "Bonus phone" in command:
+
+    if "Bonus phone" in command:
         command = command.split(" ")
         phones = command[3].split(":")
         if phones[0] in list_of_phones:
             list_of_phones.append(phones[1])
-        else:
-            continue
-    elif "Last" in command:
+
+    if "Last" in command:
         command = command.split(" ")
         if command[2] in list_of_phones:
             list_of_phones.remove(command[2])
             list_of_phones.append(command[2])
-    elif command == "End":
+
+    if command == "End":
         break
 for i in range(len(list_of_phones) - 1):
     list_of_phones[i] += ", "
